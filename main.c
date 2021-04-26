b945353e4dc8534723ba13e1fb66b6d1c3159823
Ruth-1 <ruth.adesanya@ucdconnect.ie>
2021-04-26 18:00:18 +0100

    new project othello 4

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #define X 8
 #define Y 8
+char p1[20];
+char p2[20];
 bool valid = false;
 
 struct boardSet{
@@ -17,16 +20,21 @@ struct boardSet *setBoard();
 struct boardMove *getmove(int turn);
 struct boardSet *checkmove (struct boardSet *current,struct boardMove move);
 struct boardSet *turnPiece(struct boardSet *new,struct boardMove move,struct boardMove *checkDirection);
+bool checkBounds (int row, int col);
 void printBoard(struct boardSet *current);
 void playerNames();
 int main() {
+    int turn = 1;
     playerNames();
     struct boardSet *current;
+    struct boardMove *move;
     setBoard();
     current = setBoard();
     printBoard(current);
-    int try = 'b' - 'a';
-    printf("\nTry is %d\n" ,try);
+    move = getmove(turn);
+    checkmove(current, *move);
+    printBoard(current);
+
     return 0;
 }
 struct boardSet *setBoard(){
@@ -61,8 +69,6 @@ void printBoard(struct boardSet *current){
     }
 }
 void playerNames(){
-    char p1[20];
-    char p2[20];
     printf("*** Welcome to Othello! ***\n");
     printf("Enter name of Player 1 (Black):\n");
     fgets(p1,20,stdin);
@@ -70,37 +76,34 @@ void playerNames(){
     fgets(p2,20,stdin);
 }
 struct boardMove *getmove(int turn){
-    struct boardMove *new;
+    struct boardMove *move;
     char col;
     int row;
-    new = malloc(sizeof(struct boardMove));
-    printf("%s","Player enter your move in the format 1-8 for your "
-                "desired row and a-h for your desired column\n");
-    scanf("%d" ,&row);
-    new->row = row -1;
-    scanf("%c" ,&col);
-    new->col = col - 'a';
+    char pName[20];
+    move = malloc(sizeof(struct boardMove));
     if(turn == 1){
-        new->player = 'B';
+        move->player = 'B';
+        strcpy(pName,p1);
     }
     else if(turn == 2){
-        new->player = 'W';
+        move->player = 'W';
+        strcpy(pName,p2);
     }
-
-    return(new);
+    printf("%s,%s", p1, "enter your move in the format 1-8 for your "
+                "desired row and a-h for your desired column\n");
+    scanf("%d" ,&row);
+    move->row = row - 1;
+    scanf(" %c" ,&col);
+    move->col = col - 'a';
+    printf("\n Function running well.\n");
+    printf("%d:row; %d:column.\n" ,move->row, move->col);
+    return move;
 }
 struct boardSet *checkmove(struct boardSet *current,struct boardMove move) {
     int i, j;
-    char ply;
     struct boardSet *new;
-    new = malloc(sizeof(struct boardSet));
-    struct boardMove checkDirection;
+    struct boardMove checkDirection = {0,0,0};
 
-    if (move.player == 'B') {
-        ply = 'W';
-    } else {
-        ply = 'B';
-    }
     if(current->board[move.row][move.col] != '*') {
         return NULL;
     }
@@ -135,17 +138,17 @@ struct boardSet *turnPiece(struct boardSet *new,struct boardMove move,struct boa
     }
     row = move.row + checkDirection->row;
     col = move.col + checkDirection->col;
-    if(row <X && row <= 0 && col <Y && col <= 0){
+    if(checkBounds(row,col)){
         if(new->board[row][col] == ply){
-            while(new->board[row][col] == ply && row <X && row <= 0 && col <Y && col <= 0){
+            while(new->board[row][col] == ply && checkBounds(row,col)){
                 row += checkDirection->row;
                 col +=checkDirection->col;
             }
-            if(new->board[row][col] == move.player && row <X && row <= 0 && col <Y && col <= 0 ){
+            if(new->board[row][col] == move.player && checkBounds(row,col) ){
                 row = move.row + checkDirection->row;
                 col = move.col + checkDirection->col;
                 valid = true;
-                while(new->board[row][col] == ply && row <X && row <= 0 && col <Y && col <= 0){
+                while(new->board[row][col] == ply && checkBounds(row,col)){
                     new->board[row][col] = move.player;
                     row += checkDirection->row;
                     col +=checkDirection->col;
@@ -154,4 +157,12 @@ struct boardSet *turnPiece(struct boardSet *new,struct boardMove move,struct boa
         }
     }
     return new;
+}
+bool checkBounds (int row, int col){
+    if(row <X && row >= 0 && col <Y && col >= 0){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
